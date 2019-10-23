41dc1874d23f8be3b7c2b4ea61e039d99c3b6f00
KafkaOnTheInternet <arjun_shanewarne@yahoo.com>
2019-10-23 13:18:25 +0530

    Initial commit #2

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Passenger(models.Model):
+    pass
+
+class Driver(models.Model):
+    pass
+
+
+class Ride(models.Model):
+    pass
+
+class RideDetails(models.Model):
+    pass
+
+
+
