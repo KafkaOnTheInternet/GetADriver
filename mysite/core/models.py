616481be9b7e4906b9d6fc442ac6f4a3925f654a
KafkaOnTheInternet <arjun_shanewarne@yahoo.com>
2019-11-13 13:05:49 +0530

    Appended models

--- mysite/core/models.py
+++ mysite/core/models.py
@@ -2,19 +2,42 @@ from django.db import models
 
 # Create your models here.
 
-
 class Passenger(models.Model):
-    pass
+    name = models.CharField(max_length=20)
+    birthdate = models.DateField()
+    email = models.EmailField()
+    location = models.CharField(max_length=100)
+
+    def str(self):
+        return self.name
+
 
 class Driver(models.Model):
-    pass
+    name = models.CharField(max_length=20)
+    birthdate = models.DateField()
+    email = models.EmailField()
+    location = models.CharField(max_length=100)
+
+    def str(self):
+        return self.name
 
 
 class Ride(models.Model):
-    pass
+    car = models.CharField(max_length=30)
+    price = models.IntegerField()
+    date = models.DateField()
+    passenger_name = models.ForeignKey(Passenger, on_delete=models.CASCADE)
+    driver_name = models.ForeignKey(Driver, on_delete=models.CASCADE)
+
+
 
 class RideDetails(models.Model):
     pass
+    # passenger name
+    # driver name
+    # start date
+    # end date
+    # 
 
 
 
--- mysite/core/views.py
+++ mysite/core/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Passenger
+from django.contrib.generic
 
 # Create your views here.
 
 
 def landing_view(request):
     return render(request, 'landing.html')
+
+
